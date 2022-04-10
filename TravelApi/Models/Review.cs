b2837751a7381843effae688cd575ff71b09db30
Christen Weston <paleeserecycle@gmail.com>
2022-04-10 15:55:02 -0700

    Updated Review.cs Co-authored-by: Nick Sullivan <nicholassull@gmail.com>

--- TravelApi/Models/Review.cs
+++ TravelApi/Models/Review.cs
@@ -3,15 +3,13 @@ namespace TravelApi.Models
 {
   public class Review
   {
-    // public Review()
-    // {
-    // }
+
     public int ReviewId { get; set; }
     public string Description { get; set; }
+
+    [Required]
     [Range(0, 5, ErrorMessage = "Rating must be between 0 and 5.")]
     public int Rating { get; set; }
-    public int DestinationId { get; set;}
-    public virtual Destination Destination { get; set; }
-
+    public string City { get; set; }
   }
 }
