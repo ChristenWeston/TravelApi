fbbec5ee9675e6d3b8ae62dcac580b2d993eb753
Christen Weston <paleeserecycle@gmail.com>
2022-04-10 15:51:47 -0700

    Added in Reviews Controller Co-authored-by: Nick Sullivan <nicholassull@gmail.com> Co-authored-by: Marni Sucher <marnionrails@gmail.com>

---
+++ TravelApi/Controllers/ReviewsController.cs
@@ -0,0 +1,127 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TravelApi.Models;
+using System.Linq;
+
+namespace TravelApi.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class ReviewsController : ControllerBase
+  {
+    private readonly TravelApiContext _db;
+
+    public ReviewsController(TravelApiContext db)
+    {
+      _db = db;
+    }
+
+    // GET api/animals
+    //http://localhost:5000/api/animals?species=dinosaur&gender=female
+    [HttpGet]
+    //added a parameter to the method of type string that we've called species. The naming here is important as .NET
+    // will automatically bind parameter values based on the query string. A call to http://localhost:5000/api/animals?species=dinosaur 
+    //will now trigger our Get method and automatically bind the value "dinosaur" to the variable species. It does this by utilizing model binding
+    public async Task<ActionResult<IEnumerable<Review>>> Get(int reviewId, string description, int rating, string city)
+    {
+      var query = _db.Reviews.AsQueryable();
+
+      if (description != null)
+      {
+        query = query.Where(entry => entry.Description == description);
+      }
+
+      if (rating > 0)
+      {
+        query = query.Where(entry => entry.Rating == rating);
+      }
+
+      if (city != null)
+      {
+        query = query.Where(entry => entry.City == city);
+      }
+
+      return await query.ToListAsync();
+    }
+
+    // GET: api/Animals/5
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Review>> GetReview(int id)
+    {
+      var review = await _db.Reviews.FindAsync(id);
+
+      if (review == null)
+      {
+          return NotFound();
+      }
+      return review;
+    }
+//Our POST route utilizes the function CreatedAtAction. This is so that it can end up returning the Animal object to the user
+//, as well as update the status code to 201, for "Created", rather than the default 200 OK.
+    // POST api/animals
+    [HttpPost]
+    public async Task<ActionResult<Review>> Post(Review review)
+    {
+      _db.Reviews.Add(review);
+      await _db.SaveChangesAsync();
+      return CreatedAtAction(nameof(GetReview), new { id = review.ReviewId }, review);
+    }
+
+    // PUT: api/Reviews/5
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Put(int id, Review review)
+    {
+      if (id != review.ReviewId)
+      {
+        return BadRequest();
+      }
+      _db.Entry(review).State = EntityState.Modified;
+      try
+      {
+        await _db.SaveChangesAsync();
+      }
+      catch (DbUpdateConcurrencyException)
+      {
+        if (!ReviewExists(id))
+        {
+          return NotFound();
+        }
+        else
+        {
+          throw;
+        }
+      }
+      return NoContent();
+    }
+
+// forms in HTML5 don't allow for PUT, PATCH or DELETE verbs. For that reason, we had to use HttpPost along
+// with an ActionName like this: [HttpPost, ActionName("Delete")]. However, we aren't using HTML anymore and
+// there are no such limitations with an API. For that reason, we can use HttpPut and HttpDelete.
+
+
+    // DELETE: api/Animals/5
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteReview(int id)
+    {
+      var review = await _db.Reviews.FindAsync(id);
+      if (review == null)
+      {
+        return NotFound();
+      }
+      _db.Reviews.Remove(review);
+      await _db.SaveChangesAsync();
+      return NoContent();
+    }
+
+// Private method for this controller
+    private bool ReviewExists(int id)
+    {
+      return _db.Reviews.Any(e => e.ReviewId == id);
+    }
+
+  }
+}
+
+//http://localhost:5000/api/reviews
