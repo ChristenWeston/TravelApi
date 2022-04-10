cc5394031db0ef374403b422abaa53814335dc64
Christen Weston <paleeserecycle@gmail.com>
2022-04-10 15:57:25 -0700

    Migration complete and able to get from API
    Co-authored-by: Nick Sullivan <nicholassull@gmail.com>

---
+++ TravelApi/Migrations/20220410222604_Initial.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TravelApi.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Reviews",
+                columns: table => new
+                {
+                    ReviewId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Description = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true),
+                    Rating = table.Column<int>(type: "int", nullable: false),
+                    City = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reviews", x => x.ReviewId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Reviews",
+                columns: new[] { "ReviewId", "City", "Description", "Rating" },
+                values: new object[,]
+                {
+                    { 1, "Frankfurt", "Woolly Mammoth", 5 },
+                    { 2, "Portland", "Dinosaur", 4 },
+                    { 3, "Seattle", "Dinosaur", 3 },
+                    { 4, "Cairo", "Woolly Mammoth", 4 },
+                    { 5, "Austin", "Dinosaur", 5 }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Reviews");
+        }
+    }
+}
